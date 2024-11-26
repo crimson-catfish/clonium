95bd0204065cef6f7c6115ceaa004e745a98fa6e
Rishat2 <114484896+Rishat2@users.noreply.github.com>
2024-11-26 22:06:07 +0300

    Добавлен словарь для хранения информации о комнатах и функция записи в него

--- back/supportive.py
+++ back/supportive.py
@@ -1,5 +1,8 @@
+import json
+import random
 from enum import Enum
 
+LIST_OF_INFORMATION = {}
 
 class FormOfField(Enum):
     triangle = 'triangle'
@@ -12,6 +15,9 @@ class Settings:
         self.number_of_players: int = number_of_players
         self.form_of_field: FormOfField = form_of_field
 
+    def __dict__(self):
+        return {"number_of_players": self.number_of_players, "form_of_field": self.form_of_field.value}
+
     def get_number_of_players(self) -> int:
         return self.number_of_players
 
@@ -19,3 +25,5 @@ class Settings:
         return self.form_of_field
 
 
+def write_information(room_code: int, settings: Settings) -> None:
+    LIST_OF_INFORMATION[room_code] = json.dumps(settings.__dict__())
