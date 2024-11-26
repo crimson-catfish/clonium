239f37d4f357b262713018da2c722f98046303e8
Rishat2 <114484896+Rishat2@users.noreply.github.com>
2024-11-26 18:21:19 +0300

    Удалена вспомогательная функция fetch

--- static/start.js
+++ static/start.js
@@ -1,22 +1,6 @@
 const oneDeviceButton = document.getElementById("oneDevice");
 const createRoomButton = document.getElementById("createRoom");
 const joinRoomButton = document.getElementById("joinRoom");
-let codeRoom;
-
-fetch('http://127.0.0.1:5000/api/data', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({name: "Ваня"})
-    })
-    .then(response => response.json())
-    .then(data => {
-    console.log(data);
-    })
-    .catch((error) => {
-    console.error('Ошибка:', error);
-    });
 
 async function createRoom() {
     try {
@@ -34,6 +18,6 @@ async function joinRoom() {
     }
 }
 
-console.log("script started");
+console.log("script start started");
 joinRoomButton.addEventListener("click", joinRoom);
 createRoomButton.addEventListener("click", createRoom);
