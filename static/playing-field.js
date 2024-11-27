c80862eb98194a7b3acb5233a53c20d994e75cad
Rishat2 <114484896+Rishat2@users.noreply.github.com>
2024-11-27 20:52:20 +0300

    Добавлена функция создания поля

--- static/playing-field.js
+++ static/playing-field.js
@@ -1,5 +1,7 @@
-const canvas = new fabric.Canvas('canvas');
 console.log("script started");
+const canvas = new fabric.Canvas('canvas');
+const width = 716.44;
+const height = 650;
 
 function createRadialGradient(radius, color1, color2) {
     const gradient = new fabric.Gradient({
@@ -24,7 +26,7 @@ function createHexagon(x, y, size) {
     const points = [];
 
     for (let i = 0; i < 6; i++) {
-        const angle = (i * Math.PI) / 3;
+        const angle = Math.PI / 6 + (i * Math.PI) / 3;
         points.push({
             x: x + size * Math.cos(angle),
             y: y + size * Math.sin(angle)
@@ -35,10 +37,24 @@ function createHexagon(x, y, size) {
         fill: createRadialGradient(size, 'lightblue', '#4285B4'),
         left: x,
         top: y,
+        /*strokeLineJoin: 'round',
+        strokeWidth: 2,
+        stroke: '#4285B4',*/
         selectable: false
     });
     return hexagon;
 }
 
-const hexagon = createHexagon(100, 100, 50);
-canvas.add(hexagon);
+function createField() {
+    size = height / 11;
+
+    for (let y = 0; y < 9 * size + 0.1; y += 1.5 * size) {
+        let offset = Math.abs(y / (1.5 * size) - 3) * (Math.sqrt(3) / 2) * size;
+        for (let x = offset; x < width - offset - 0.1; x += Math.sqrt(3) * size) {
+            add(createHexagon(x, y, size));
+        }
+    }
+}
+
+createField();
+
